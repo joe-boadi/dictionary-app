b1084d577912f6e8a2fa16097198d9548c152895
Kwadjo Boadi-Mantey <kwadjo.boadi-mantey@amalitech.com>
2024-11-22 16:57:27 +0000

    chore: Update styles of page

--- main.js
+++ main.js
@@ -111,3 +111,15 @@ searchInput.addEventListener("keydown", (event) => {
       handleSearch();
     }
   });
+
+  // Handle background mode
+const moon = document.getElementById('moon')
+// const moonIcon = document.getElementsById('moon-icon')
+const toggleBackground = document.getElementById('checkbox') 
+
+toggleBackground.addEventListener('click', ()=>{
+    let background = document.body;
+    background.classList.toggle('dark-mode')
+    moon.style.color = '#A445ED'
+    moon.style.f = '#A445ED'
+})
